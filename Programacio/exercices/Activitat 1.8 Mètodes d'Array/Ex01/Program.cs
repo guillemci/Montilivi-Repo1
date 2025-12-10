3100467248434ac57cc4247a51bcc127369e4370
guillemci <guillemcib@gmail.com>
2025-12-10 22:36:47 +0100

    exercise 1 added

---
+++ Programacio/exercices/Activitat 1.8 Mètodes d'Array/Ex01/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Ex01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("especifica la longitud del array");
+            int longitudArray = Convert.ToInt32(Console.ReadLine());
+            int[] ArrayReturnat = OmpleNaturals(longitudArray);
+
+            Console.WriteLine("els numeros naturals del array son");
+            for (int i = 0; i < ArrayReturnat.Length; i++)
+            {
+                Console.WriteLine(ArrayReturnat[i]);
+            }
+        }
+
+        public static int[] OmpleNaturals(int n)
+        {
+            int[] numNatu = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                numNatu[i] = i + 1;
+            }
+            return numNatu;
+
+        }
+    }
+}
