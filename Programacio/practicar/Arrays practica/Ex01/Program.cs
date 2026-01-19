bcc279e8403fd0cbecb56a0231b75cf0683eaab3
guillemci <guillemcib@gmail.com>
2026-01-19 17:37:25 +0100

    refresh

---
+++ Programacio/practicar/Arrays practica/Ex01/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Ex01
+{
+    internal class Program
+    {
+        /// <summary>
+        /// Exercici 01
+        /// Retorna un array de longitud 'n' amb els N primers números naturals.
+        /// Exemple: n=3 -> [1, 2, 3]
+        /// </summary>
+        static void Main(string[] args)
+        {
+            int n = 3;
+
+            int[] hola = OmpleNaturals(n);
+
+            for (int i = 0; i < hola.Length; i++)
+                Console.WriteLine(hola[i]);
+        }
+
+        public static int[] OmpleNaturals(int n)
+        {
+            int[] hola = new int[n];
+
+            for (int i = 0; i < n; i++)
+                hola[i] = i + 1;
+
+            return hola;
+        }
+    }
+}
---
+++ Programacio/practicar/Arrays practica/Ex02/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Ex02
+{
+    internal class Program
+    {
+        /// <summary>
+        /// /// Exercici 02
+        /// Retorna un array de longitud 'n' amb números enters aleatoris entre -100 i 100.
+        /// </summary>
+        static void Main(string[] args)
+        {
+
+        }
+
+        public static int[] GenerarTaula(int n)
+        {
+
+        }
+    }
+}
