4edc7b15fa6cb806d7a29b80455729fffd1a66d0
guillemci <guillemcib@gmail.com>
2026-02-01 22:33:10 +0100

    tasca js 3 començada

--- Programacio/practicar/Arrays practica/Ex02/Program.cs
+++ Programacio/practicar/Arrays practica/Ex02/Program.cs
@@ -8,12 +8,25 @@
         /// </summary>
         static void Main(string[] args)
         {
+            int[] array = new int[] { 2, 4, 5, 6, 10, 2 };
 
+            RotarArray(array);
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i]);
+            }
         }
 
-        public static int[] GenerarTaula(int n)
+        public static void RotarArray(int[] array)
         {
-
+            int cacheArray;
+            for (int i = 0; i < array.Length / 2; i++)
+            {
+                cacheArray = array[(array.Length - 1) - i];
+                array[(array.Length - 1) - i] = array[i];
+                array[i] = cacheArray;
+            }
         }
     }
 }
---
+++ Programacio/practicar/Arrays practica/Ex03/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Ex03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 1, 5, 4 };
+
+            int total = caluclarArrayNumeros(array);
+
+            Console.WriteLine(total);
+        }
+
+        static int caluclarArrayNumeros(int[] array)
+        {
+            int sumarTotal = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                sumarTotal += array[i];
+            }
+
+            return sumarTotal;
+        }
+    }
+}
